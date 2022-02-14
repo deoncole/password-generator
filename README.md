464affd7380f73139208602f2998605ea676d9e0
Cole <deoncole77@gmail.com>
2022-02-13 22:24:16 -0500

    add functions to create and display on screen

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Password Generator
+
+## Password generator can allow the users to select how long their password should be, between 8-128 characters. If the user doesn't choose between the two an alert is provided telling the user what values are accepted. The user can choose between special characters, upper and/or lowercase letters, and numbers. Once the choices are made, the new password is displayed on the screen.
+
+## Link to deloy application: https://deoncole.github.io/password-generator/
+
+## ![Alt text](/assets/images/password-generator-index-html.png?raw=true "Optional Title")
--- script.js
+++ script.js
@@ -12,9 +12,13 @@ var passwordInfo = {
   infoArray: []
 };
 
+//boolean to check if the object's values need to be reset
+var resetReady = true;
+
 // create a function to check that the input for password length is between 8 and 128
 var passwordLength = function (lengthCheck){
-  if (lengthCheck < 8 || lengthCheck === ""){
+  debugger;
+  if (lengthCheck < 8){
     window.alert("Your password cannot be less than 8 characters");
     generatePassword();
   } else if (lengthCheck > 128){
@@ -22,10 +26,11 @@ var passwordLength = function (lengthCheck){
     generatePassword();
   } else {
     passwordInfo.count = lengthCheck;
-    console.log("You chose to have your pasword " + passwordInfo.count + " long!");
+    resetReady = false;
   };
 };
 
+//create a function to confirm what the user wants and store the info into the object infoArray using a for loop in a if statement
 var putConfirmInfo = function (confirmInfo, isUpperCase){
   if (isUpperCase === false){
     for (var i = 0; i<confirmInfo.length; i++){
@@ -38,6 +43,7 @@ var putConfirmInfo = function (confirmInfo, isUpperCase){
   };
 };
 
+//create a function to confirm if the user wants to use special charaters in their password using a prompt and a if statement and store the values within the object
 var checkSpecialChar = function (){
   var putSpecChar = window.confirm("Would you like to add special characters to your password?");
   
@@ -50,6 +56,7 @@ var checkSpecialChar = function (){
   return;
 };
 
+//create a function to confirm if the user wants to use lower case charaters in their password using a prompt and a if statement and the values within the object
 var addLowerCase = function (){
   var putLowerCase = window.confirm("Would you like to add Lowercase letters to your password?");
 
@@ -62,9 +69,10 @@ var addLowerCase = function (){
   return;
 };
 
+//create a function to confirm if the user wants to use lower case charaters in their password using a prompt and a if statement and the values within the object
 var addUpperCase = function (){
   var putUpperCase = window.confirm("Would you like to add Uppercase letters to your password?");
-  debugger;
+  
   if (putUpperCase) {
     passwordInfo.addUpperCase = true;
     putConfirmInfo(passwordInfo.alpha, true);
@@ -74,13 +82,13 @@ var addUpperCase = function (){
   return;
 };
 
+//create a function to confirm if the user wants to use lower case charaters in their password using a prompt and a if statement and the values within the object
 var addNumbers = function (){
   var putNumbers = window.confirm("Would you like to add Numbers to your password?");
 
   if (putNumbers) {
     passwordInfo.addNumber = true;
     putConfirmInfo(passwordInfo.numbers, false);
-    console.log(passwordInfo.infoArray)
   } else{
     passwordInfo.addUpperCase = false;
   };
@@ -90,33 +98,62 @@ var addNumbers = function (){
 
 //create a function to start the password generation process
 var generatePassword = function (){
-  var passwordCount = window.prompt("How long would you like your password to be? \r\n Type a number between 8 - 128")
-  // console.log(passwordCount);
+  var passwordCount = window.prompt("How long would you like your password to be? \r\n Type a number between 8 - 128");
   
+  //call the various functions to get the input/selections from the user. Pass the input from the passwordCount into the password length function to check the amount chosen
   passwordLength(passwordCount);
-
   checkSpecialChar();
   addLowerCase();
   addUpperCase();
-  addNumbers();
-  return;
+  addNumbers(); 
+
+  //local empty array to hold the selected characters
+  var newPassword = [];
+
+  //use a for loop to generate the random characters for the password based upon the user's selection
+  for(var i=0; i < passwordInfo.count; i++){
+    var anyChar = Math.floor(Math.random() * passwordInfo.infoArray.length);
+    //insert the random selected characters into the local empty array
+    newPassword.push(passwordInfo.infoArray[anyChar]);
+  }
+
+  //variable to hold the new completed password, use the to string method to set it as a tring and the replace all method to remove the commas
+  var completePass = newPassword.toString().replaceAll(",", "");
+  
+  //return the completed password
+  return completePass;
 };
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // Don't need the for loop right now
-  // for (var i = 0; i < passwordInfo.alpha.length; i++ ){
-  //   var anyNum = Math.floor(Math.random() * passwordInfo.alpha.length);
-  //   console.log(passwordInfo.alpha[anyNum])
-  // }
 
-  generatePassword();
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+  //if statement to check if the objects need to be reset or not
+  if(resetReady){
+    //get the password from the generatePassword function
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  // passwordText.value = password;
+    //set the password to the text field
+    passwordText.value = password;
+
+  } else {
+    passwordInfo.count = "";
+    passwordInfo.addSpecChar = "";
+    passwordInfo.addLowerCase = "";
+    passwordInfo.addUpperCase = "";
+    passwordInfo.addNumber = "";
+    passwordInfo.infoArray = [];
+    resetReady = true;
+    //get the password from the generatePassword function
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    //set the password to the text field
+    passwordText.value = password;
+  };
 
 }
 
